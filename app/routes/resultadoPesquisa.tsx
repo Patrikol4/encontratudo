79cdff0e59f04455ce78b9fe783e185fd719e1ff
Patrick Souza <doublecrt@gmail.com>
2023-09-06 16:37:39 -0300

    improved ResultadoPesquisa

--- app/routes/resultadoPesquisa.tsx
+++ app/routes/resultadoPesquisa.tsx
@@ -181,6 +181,11 @@ export default function ResultadoPesquisa() {
                   Resultado(S) da Pesquisa
                 </span>
               </h1>
+              <h3 className="text-center text-2xl font-bold tracking-tighter sm:text-2xl lg:text-2xl sm:mb-4 md:mb-4 lg:mb-4">
+              <span className="block uppercase text-white drop-shadow-md">
+                  Teresina - PI
+                </span>
+              </h3>
 
               <div className="mx-auto max-w-7xl px-4 py-2 sm:px-6 lg:px-8 mb-8">
 
